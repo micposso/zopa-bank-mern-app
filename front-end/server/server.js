70ae606e12bb0b52927a94072d4ede07e51a8af2
unknown <micposso@gmail.com>
2021-09-19 00:19:30 -0400

    add DAL

--- front-end/server/server.js
+++ front-end/server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // create user account
 
-app.get('/account/create/:name/:email/:password', function (req, res) {
+app.get('/create/:name/:email/:password', function (req, res) {
   res.send({
     name: req.params.name,
     email: req.params.email,
